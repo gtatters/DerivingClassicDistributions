edb785cc8798bf1e56db44fb450f3fa33c99c51b
Glenn Tattersall <11757426+gtatters@users.noreply.github.com>
2026-06-21 10:03:17 -0400

    Added sample r code to explainer text

--- app.R
+++ app.R
@@ -178,67 +178,83 @@ server <- function(input, output, session) {
     
     if (input$dist == "t") {
       
-      t_vals <- t_data()
+      t_vals    <- t_data()
       prev_vals <- attr(t_vals, "prev")
-      df <- input$n_t - 1
+      df        <- input$n_t - 1
       
       breaks <- pretty(range(c(t_vals, prev_vals), na.rm = TRUE), n = 30)
+      h_curr <- hist(t_vals, breaks = breaks, plot = FALSE)
+      ylim   <- c(0, max(h_curr$density) * 1.15)
+      
+      title_txt <- paste("t distribution (n =", input$n_t, ", df =", df, ")")
       
       if (!is.null(prev_vals)) {
         hist(prev_vals, probability = TRUE, breaks = breaks,
              col = rgb(0.7, 0.7, 0.7, 0.4), border = NA,
-             main = paste("t distribution (n =", input$n_t, ")"),
-             xlab = "t statistic")
+             main = title_txt, xlab = "t statistic", ylim = ylim)
+        hist(t_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black", add = TRUE)
+      } else {
+        hist(t_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black",
+             main = title_txt, xlab = "t statistic", ylim = ylim)
       }
-      
-      hist(t_vals, probability = TRUE, breaks = breaks,
-           col = "#569BBD", border = "black",
-           add = !is.null(prev_vals))
       
       curve(dt(x, df = df), add = TRUE, col = "red", lwd = 2)
       
     } else if (input$dist == "f") {
       
-      f_vals <- f_data()
+      f_vals    <- f_data()
       prev_vals <- attr(f_vals, "prev")
-      df1 <- input$n1_f - 1
-      df2 <- input$n2_f - 1
+      df1       <- input$n1_f - 1
+      df2       <- input$n2_f - 1
       
       breaks <- pretty(range(c(f_vals, prev_vals), na.rm = TRUE), n = 30)
+      h_curr <- hist(f_vals, breaks = breaks, plot = FALSE)
+      ylim   <- c(0, max(h_curr$density) * 1.15)
+      
+      title_txt <- paste("F distribution (df1 =", df1, ", df2 =", df2, ")")
       
       if (!is.null(prev_vals)) {
         hist(prev_vals, probability = TRUE, breaks = breaks,
              col = rgb(0.7, 0.7, 0.7, 0.4), border = NA,
-             main = paste("F distribution (df1 =", df1, ", df2 =", df2, ")"),
-             xlab = "F statistic", xlim = c(0, 10))
+             main = title_txt, xlab = "F statistic",
+             xlim = c(0, 10), ylim = ylim)
+        hist(f_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black", add = TRUE, xlim = c(0, 10))
+      } else {
+        hist(f_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black",
+             main = title_txt, xlab = "F statistic",
+             xlim = c(0, 10), ylim = ylim)
       }
-      
-      hist(f_vals, probability = TRUE, breaks = breaks,
-           col = "#569BBD", border = "black",
-           add = !is.null(prev_vals), xlim = c(0, 10))
       
       curve(df(x, df1 = df1, df2 = df2), add = TRUE, col = "red", lwd = 2)
       
     } else {
       
-      chi_vals <- chi_data()
+      chi_vals  <- chi_data()
       prev_vals <- attr(chi_vals, "prev")
       
       breaks <- pretty(range(c(chi_vals, prev_vals), na.rm = TRUE), n = 30)
+      h_curr <- hist(chi_vals, breaks = breaks, plot = FALSE)
+      ylim   <- c(0, max(h_curr$density) * 1.15)
+      
+      title_txt <- paste("Chi-square distribution (df =", input$k_chi, ")")
       
       if (!is.null(prev_vals)) {
         hist(prev_vals, probability = TRUE, breaks = breaks,
              col = rgb(0.7, 0.7, 0.7, 0.4), border = NA,
-             main = paste("Chi-square distribution (df =", input$k_chi, ")"),
-             xlab = expression(chi^2))
+             main = title_txt, xlab = expression(chi^2), ylim = ylim)
+        hist(chi_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black", add = TRUE)
+      } else {
+        hist(chi_vals, probability = TRUE, breaks = breaks,
+             col = "#569BBD", border = "black",
+             main = title_txt, xlab = expression(chi^2), ylim = ylim)
       }
       
-      hist(chi_vals, probability = TRUE, breaks = breaks,
-           col = "#569BBD", border = "black",
-           add = !is.null(prev_vals))
-      
-      curve(dchisq(x, df = input$k_chi),
-            add = TRUE, col = "red", lwd = 2)
+      curve(dchisq(x, df = input$k_chi), add = TRUE, col = "red", lwd = 2)
     }
   })
   
@@ -249,25 +265,49 @@ server <- function(input, output, session) {
     
     if (input$dist == "t") {
       paste0(
-        "t distribution: emerges from drawing n samples from a normal distribution\nand standardizing sample means: (mean/sd/sqrt(n_t)).\n",
-        "Red line shows the theoretical t distribution densities for comparison.\n",
-        "As n increases, it approaches the standard normal distribution.\n"
+        "t distribution: emerges from drawing n samples from a normal distribution\n",
+        "and standardizing sample means: t = mean(x) / (sd(x) / sqrt(n)).\n",
+        "Red line shows the theoretical t distribution for comparison.\n",
+        "As n increases, the t distribution approaches the standard normal.\n\n",
+        "--- R code used to generate these t values ---\n",
+        "n      <- ", input$n_t, "   # sample size\n",
+        "n_sims <- ", if (!is.null(input$n_exp_t)) input$n_exp_t else 1000, "   # number of simulations\n",
+        "t_vals <- replicate(n_sims, {\n",
+        "  x <- rnorm(n)\n",
+        "  mean(x) / (sd(x) / sqrt(n))\n",
+        "})\n"
       )
       
     } else if (input$dist == "f") {
       paste0(
         "F distribution: emerges from the ratio of variances of two independent normal samples.\n",
         "Degrees of freedom: df1 = n1-1 (numerator), df2 = n2-1 (denominator).\n",
-        "Red lines show the theoretical t and F distribution densities for comparison.\n",
-        "Used extensively in ANOVA and variance comparison."
+        "Red line shows the theoretical F distribution for comparison.\n",
+        "Used extensively in ANOVA and regression to compare group variances.\n\n",
+        "--- R code used to generate these F values ---\n",
+        "n1     <- ", input$n1_f, "   # sample size group 1\n",
+        "n2     <- ", input$n2_f, "   # sample size group 2\n",
+        "n_sims <- ", if (!is.null(input$n_exp_f)) input$n_exp_f else 1000, "   # number of simulations\n",
+        "f_vals <- replicate(n_sims, {\n",
+        "  x1 <- rnorm(n1)\n",
+        "  x2 <- rnorm(n2)\n",
+        "  var(x1) / var(x2)\n",
+        "})\n"
       )
       
     } else {
       paste0(
         "Chi-square distribution: emerges from summing squared standard normal variables.\n",
-        "If Z ~ N(0,1), then χ²(k) = sum(Z^2) over k independent draws.\n",
-        "Here, we simulate k standard normals and square + sum them.\n",
-        "This connects directly to variance estimation and likelihood theory.\n"
+        "If Z ~ N(0,1), then chi-sq(k) = sum(Z^2) over k independent draws.\n",
+        "Red line shows the theoretical chi-square distribution for comparison.\n",
+        "Connects directly to variance estimation, goodness-of-fit, and contingency tables.\n\n",
+        "--- R code used to generate these chi-square values ---\n",
+        "k      <- ", input$k_chi, "   # degrees of freedom\n",
+        "n_sims <- ", if (!is.null(input$n_exp_chi)) input$n_exp_chi else 1000, "   # number of simulations\n",
+        "chi_vals <- replicate(n_sims, {\n",
+        "  z <- rnorm(k)\n",
+        "  sum(z^2)\n",
+        "})\n"
       )
     }
   })
